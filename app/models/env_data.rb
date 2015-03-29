f73919ee171a3d7637a1c0e47df1c4a3e61b5471
Sue Lucas <choi.soyoung@gmail.com>
2015-03-28 22:41:31 -0700

    add dependent destroy & validations on EnvData

--- app/models/env_data.rb
+++ app/models/env_data.rb
@@ -1,3 +1,4 @@
 class EnvData < ActiveRecord::Base
-  has_many :outfits
+  has_many :outfits, dependent: :destroy
+  validates :location, :weather_temp, :weather_state, :date, :user_id, presence: true
 end
