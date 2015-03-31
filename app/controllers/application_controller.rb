c4522bf68388a0b78471e02badd828a5d2dcd091
Sue Lucas <choi.soyoung@gmail.com>
2015-03-31 09:50:53 -0700

    passing @user instance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    styles_path(@user)
+    style_path(@user)
   end
 
   # Prevent CSRF attacks by raising an exception.
--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,9 +1,11 @@
 class StylesController < ApplicationController
   before_action :authenticate_user!
   def index
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
