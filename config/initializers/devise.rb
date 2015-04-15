ad2e7b17ae6cb39b4d690e8c8188df7e363fd397
Christine Lau <chylau.design@gmail.com>
2015-04-14 19:54:41 -0700

    modal styling

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,14 +4,14 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = '8445debbe4277e00f376cafb888f330bbbdf04316cb9c82b1f7c5f95da4a0e28d7e386687dc7a39e283e644a6461be680b5f3e63104ee9f895e46a7f5efbb6ae'
+  config.secret_key = '8445debbe4277e00f376cafb888f330bbbdf04316cb9c82b1f7c5f95da4a0e28d7e386687dc7a39e283e644a6461be680b5f3e63104ee9f895e46a7f5efbb6ae'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  # config.assets.initialize_on_precompile = false
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
