30c8c87ab0152dd84a9fef73311a3d94dab4883a
Christine Lau <chylau.design@gmail.com>
2016-02-09 13:57:59 -0800

    https stuff

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -30,8 +30,8 @@ $(document).ready(function () {
     access_2Id = detailsDiv.find("#access_2Id").children("img")[0].alt;
 
   if (dressId !== undefined && dressId !== null) {
-    var modalDressURL = "http://api.shopstyle.com/api/v2/products/"+dressId+"?pid=uid2100-27524390-36";
-    $.getJSON(modalDressURL, function(data) {      
+    var modalDressURL = "https://api.shopstyle.com/api/v2/products/"+dressId+"?pid=uid2100-27524390-36";
+    $.getJSON(modalDressURL, function(data) {
       $('.modalDress').append("<img src="+data.image.sizes.Large.url+"><br>");
       $('.modalDress').append("<p>"+data.brand.name+"</p>");
       $('.modalDress').append("<p>"+data.unbrandedName+"</p>");
@@ -39,7 +39,7 @@ $(document).ready(function () {
   }
 
   if (topId !== undefined && topId !== null) {
-    var modalTopURL = "http://api.shopstyle.com/api/v2/products/"+topId+"?pid=uid2100-27524390-36";
+    var modalTopURL = "https://api.shopstyle.com/api/v2/products/"+topId+"?pid=uid2100-27524390-36";
     $.getJSON(modalTopURL, function(data) {
       $('.modalTop').append("<img src="+data.image.sizes.Large.url+"><br>");
       $('.modalTop').append("<p>"+data.brand.name+"</p>");
@@ -48,7 +48,7 @@ $(document).ready(function () {
   }
 
   if (bottomId !== undefined && bottomId !== null) {
-    var modalBottomURL = "http://api.shopstyle.com/api/v2/products/"+bottomId+"?pid=uid2100-27524390-36";
+    var modalBottomURL = "https://api.shopstyle.com/api/v2/products/"+bottomId+"?pid=uid2100-27524390-36";
     $.getJSON(modalBottomURL, function(data) {
       $('.modalBottom').append("<img src="+data.image.sizes.Large.url+"><br>");
       $('.modalBottom').append("<p>"+data.brand.name+"</p>");
@@ -57,7 +57,7 @@ $(document).ready(function () {
   }
 
   if (shoeId !== undefined && shoeId !== null) {
-    var modalShoeURL = "http://api.shopstyle.com/api/v2/products/"+shoeId+"?pid=uid2100-27524390-36";
+    var modalShoeURL = "https://api.shopstyle.com/api/v2/products/"+shoeId+"?pid=uid2100-27524390-36";
     $.getJSON(modalShoeURL, function(data) {
       $('.modalShoe').append("<img src="+data.image.sizes.Large.url+"><br>");
       $('.modalShoe').append("<p>"+data.brand.name+"</p>");
@@ -66,7 +66,7 @@ $(document).ready(function () {
   }
 
   if (access_1Id !== undefined && access_1Id !== null) {
-    var modalAccess1URL = "http://api.shopstyle.com/api/v2/products/"+access_1Id+"?pid=uid2100-27524390-36";
+    var modalAccess1URL = "https://api.shopstyle.com/api/v2/products/"+access_1Id+"?pid=uid2100-27524390-36";
     $.getJSON(modalAccess1URL, function(data) {
       $('.modalAccess1').append("<img src="+data.image.sizes.Large.url+"><br>");
       $('.modalAccess1').append("<p>"+data.brand.name+"</p>");
@@ -75,7 +75,7 @@ $(document).ready(function () {
   }
 
   if (access_2Id !== undefined && access_2Id !== null) {
-    var modalAccess2URL = "http://api.shopstyle.com/api/v2/products/"+access_2Id+"?pid=uid2100-27524390-36";
+    var modalAccess2URL = "https://api.shopstyle.com/api/v2/products/"+access_2Id+"?pid=uid2100-27524390-36";
     $.getJSON(modalAccess2URL, function(data) {
       $('.modalAccess2').append("<img src="+data.image.sizes.Large.url+"><br>");
       $('.modalAccess2').append("<p>"+data.brand.name+"</p>");
